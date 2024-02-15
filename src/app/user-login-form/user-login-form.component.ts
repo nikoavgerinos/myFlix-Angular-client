caa9a356546d4c46cc7a75e9abb05e2f9c46617e
nikoavgerinos <avgerinos.niko@gmail.com>
2024-02-15 13:10:22 +0100

    chore(auth): enable Enter key for login and registration forms

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,13 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
-
-// Import to close the dialog on success
 import { MatDialogRef } from '@angular/material/dialog';
-
-// Import to bring in the API call created in 6.2
-import { FetchApiDataService  } from '../fetch-api-data.service';
-
-// Import to display notifications back to the user
+import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
@@ -15,28 +9,26 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   templateUrl: './user-login-form.component.html',
   styleUrls: ['./user-login-form.component.scss'],
 })
-export class UserLoginFormComponent implements OnInit{
+export class UserLoginFormComponent implements OnInit {
 
   @Input() userData = { Username: '', Password: '' }
 
   constructor(
-    public fetchApiData: FetchApiDataService ,
+    public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
     public snackBar: MatSnackBar,
     private router: Router
   ) { }
-  
+
   ngOnInit(): void {
   }
 
-  // Function responsible for sending the form inputs to the backend
   loginUser(): void {
     this.fetchApiData.userLogin(this.userData).subscribe((result) => {
-      //Logic for a successful user login
       console.log(result);
       localStorage.setItem('user', JSON.stringify(result.user));
       localStorage.setItem('token', result.token);
-      this.dialogRef.close(); // Will close modal on success
+      this.dialogRef.close();
       this.snackBar.open('User login successful', 'OK', {
         duration: 2000
       });
@@ -49,4 +41,12 @@ export class UserLoginFormComponent implements OnInit{
     });
   }
 
+  // Listen for the keyup event on the document
+  @HostListener('document:keyup', ['$event'])
+  handleKeyUp(event: KeyboardEvent): void {
+    // Check if the Enter key is pressed (keyCode 13)
+    if (event.key === 'Enter') {
+      this.loginUser(); // Call your loginUser function when Enter key is pressed
+    }
+  }
 }
--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -1,15 +1,7 @@
-// src/app/user-registration-form/user-registration-form.component.ts
-import { Component, OnInit, Input } from '@angular/core';
-
-// You'll use this import to close the dialog on success
+import { Component, OnInit, Input, HostListener } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
-
-// This import brings in the API calls we created in 6.2
 import { FetchApiDataService } from '../fetch-api-data.service';
-
-// This import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
-
 
 @Component({
   selector: 'app-user-registration-form',
@@ -20,22 +12,22 @@ export class UserRegistrationFormComponent implements OnInit {
 
   @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
 
-constructor(
+  constructor(
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
-    public snackBar: MatSnackBar) { }
+    public snackBar: MatSnackBar
+  ) { }
 
-ngOnInit(): void {
-}
+  ngOnInit(): void {
+  }
 
-// This is the function responsible for sending the form inputs to the backend
-registerUser(): void {
+  // Function responsible for sending the form inputs to the backend
+  registerUser(): void {
     this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
-  // Logic for a successful user registration goes here! (To be implemented)
-     this.dialogRef.close(); // This will close the modal on success!
-     this.snackBar.open(result, 'OK', {
+      this.dialogRef.close();
+      this.snackBar.open(result, 'OK', {
         duration: 2000
-     });
+      });
     }, (result) => {
       this.snackBar.open(result, 'OK', {
         duration: 2000
@@ -43,4 +35,12 @@ registerUser(): void {
     });
   }
 
+  // Listen for the keyup event on the document
+  @HostListener('document:keyup', ['$event'])
+  handleKeyUp(event: KeyboardEvent): void {
+    // Check if the Enter key is pressed (keyCode 13)
+    if (event.key === 'Enter') {
+      this.registerUser(); // Call your registerUser function when Enter key is pressed
+    }
   }
+}
